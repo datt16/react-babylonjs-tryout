dd874f06b6307a9b9f6a83929af66fa453d6a74f
datt16 <osatom.4639@gmail.com>
2022-07-08 14:36:05 +0900

    メモ追記

--- src/BoxScene.tsx
+++ src/BoxScene.tsx
@@ -11,18 +11,21 @@ export const BoxScene = () => {
     <div className='wrapper'>
       <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
         <Scene>
+          {/* カメラ... これがないと画面に何も表示されない */}
           <freeCamera
             name='camera'
             position={new Vector3(0, 5, -10)}
             setTarget={[Vector3.Zero()]}
           />
 
+          {/* ライト... これがないと真っ暗で色が分からない */}
           <hemisphericLight
             name='light'
             direction={Vector3.Up()}
             intensity={1.2}
           />
 
+          {/* 箱... 箱本体, (0,0,0)の位置に2m四方の立方体を生成 */}
           <box name='box' ref={boxRef} size={2} position={new Vector3(0, 0, 0)}>
             <standardMaterial
               name='box-material'
